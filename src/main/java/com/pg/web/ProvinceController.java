ca86f8976a364aa5d7fc79eebf4aed80158bf7f1
lyzzyz@gmail.com <lyzzyz@gmail.com>
2015-03-19 15:17:36 +0800

    Initial commit

---
+++ src/main/java/com/pg/web/ProvinceController.java
@@ -0,0 +1,30 @@
+package com.pg.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.core.service.ICommonService;
+import com.pg.entity.Province;
+
+@Controller
+public class ProvinceController {
+
+	@Autowired
+	@Qualifier("CommonService")
+	private ICommonService commonServiec;
+
+	@RequestMapping(value = "/abs")
+
+	public String list(Model model) {
+		final Province province = commonServiec.get(Province.class, 26l);
+		System.out.println(province);
+        province.setProName("李喻哲");
+        province.update();
+        System.out.println(province);
+		return "index.jsp";
+	}
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: yzlik
+  Date: 2015/3/19
+  Time: 13:34
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+----你好
+  </body>
+</html>
